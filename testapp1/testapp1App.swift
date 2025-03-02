5acf653c974d70529e5204f18a0ff9a53791e68e
Raheem <rchisman@microsoft.com>
2025-03-02 09:10:25 -0800

    Initial Commit

---
+++ testapp1/testapp1App.swift
@@ -0,0 +1,17 @@
+//
+//  testapp1App.swift
+//  testapp1
+//
+//  Created by Raheem Chisman on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct testapp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testapp1Tests/testapp1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  testapp1Tests.swift
+//  testapp1Tests
+//
+//  Created by Raheem Chisman on 3/2/25.
+//
+
+import Testing
+@testable import testapp1
+
+struct testapp1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
